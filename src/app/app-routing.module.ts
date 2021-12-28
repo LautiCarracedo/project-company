2b4240e75b187011c7508ca1495bd5077addfa22
Lauti Carracedo <lauti.carracedo@hotmail.com>
2021-12-27 21:51:24 -0300

    front home and carga asegurados

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AseguradosComponent } from './asegurados/asegurados.component';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path:'', component: HomeComponent },
+  { path:'asegurados', component: AseguradosComponent }
+];
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    [RouterModule.forRoot(routes)]
+  ],
+  exports:[RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AseguradosComponent } from './asegurados/asegurados.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ConsultasComponent } from './consultas/consultas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent,
+    AseguradosComponent,
+    HomeComponent,
+    ConsultasComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
